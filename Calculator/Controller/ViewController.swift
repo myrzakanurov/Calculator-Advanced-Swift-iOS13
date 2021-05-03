bd0ecc37ba469fbfd301488f0f7eca81aba62b41
Myrzakanurov Arman <myrzakanurov.a@gmail.com>
2021-05-03 16:24:57 +0600

    Completed Version

---
+++ Calculator/Controller/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Angela Yu on 10/09/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var isFinishedTypingNumber: Bool = true
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text into double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var calculator = CalculatorLogic()
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = false
+        
+        calculator.setNumber(displayValue)
+        
+        if let calcMethods = sender.currentTitle {
+            
+            if let result = calculator.calculate(symbol: calcMethods) {
+                displayValue = result
+            }
+        }
+    
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            if !isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text?.append(numValue)
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+            }
+        }
+    
+    }
+
+}
+
